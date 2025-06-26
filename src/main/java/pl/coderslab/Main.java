cd7210ac4a620d4fc1fe91c8168d55750807d845
apruszko-v <apruszko@visa.com>
2025-06-26 23:54:32 +0200

    add methods: selectOptions, addOption, removeOption, listOption, exitOption

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,11 +1,76 @@
 package pl.coderslab;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        selectOption();
+    }
+
+    public static void selectOption() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(ConsoleColors.PURPLE_BACKGROUND + ConsoleColors.BLACK_BOLD + "Please select an option");
+        System.out.println(ConsoleColors.PURPLE + """
+                add
+                remove
+                list
+                exit
+                """);
+
+        String userInput = scanner.nextLine();
+
+        switch (userInput) {
+            case "add" -> addOption();
+            case "remove" -> removeOption();
+            case "list" -> listOption();
+            case "exit" -> exitOption();
         }
     }
+
+
+    public static void addOption() {
+//      add logika: dodanie taska z tymi parametrami co ktos wpisal do pliku tasks.csv
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(ConsoleColors.GREEN_BACKGROUND + ConsoleColors.BLACK_BOLD + "Please add task description");
+        String taskDescription = scanner.next();
+
+        System.out.println(ConsoleColors.GREEN_BACKGROUND + ConsoleColors.BLACK_BOLD + "Please add task due date");
+        String taskDueDate = scanner.next();
+
+        System.out.println(ConsoleColors.GREEN_BACKGROUND + ConsoleColors.BLACK_BOLD + "Is your task important? true/false");
+        String taskImportance = scanner.next();
+
+        selectOption();
+    }
+
+    public static void removeOption() {
+//        add logika: usuwanie taska o numerze X
+//            add logika: usuniecie taska z pliku tasks.csv
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(ConsoleColors.YELLOW_BACKGROUND_BRIGHT + ConsoleColors.BLACK_BOLD_BRIGHT + "Please select number to remove");
+        String removeNumber = scanner.next();
+
+        System.out.println(ConsoleColors.RED_UNDERLINED + "Value was successfully deleted");
+
+        selectOption();
+    }
+
+    public static void listOption() {
+//          add logika: printowanie w konsoli taskow ktore zostaly dodane
+//            add logika: zapis do pliku tasks.csv
+        selectOption();
+    }
+
+    public static void exitOption() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        if (userInput.equals("exit")) {
+            System.out.println(ConsoleColors.YELLOW_UNDERLINED + "Bye, bye.");
+        }
+    }
+}
